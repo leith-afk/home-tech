5c5bb22b2a0481fc8bec29818b2b0cf39524d877
leith-afk <leith@hotelaat.com>
2026-01-23 02:58:48 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Home Tech</h1>
+
+      <p>Welcome to Home Tech</p>
+      <p>مرحبًا بكم في هوم تك</p>
+
+      <button style={{ marginRight: 10 }}>English</button>
+      <button>العربية</button>
+    </main>
+  );
+}
